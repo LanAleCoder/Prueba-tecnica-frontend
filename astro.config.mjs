aedc83930a91c6ebdb4cafc876f5be7aac7ce866
LanAleCoder <peter24alexander@gmail.com>
2024-05-31 15:52:27 -0600

    configuración para las imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
