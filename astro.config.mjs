e8aa1bdded5e2e6dab4096262b54da5909ee43a8
LanAleCoder <peter24alexander@gmail.com>
2024-05-31 15:58:18 -0600

    instalación de sharp para las imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService(),
-  },
   output: "server",
-  adapter: vercel(),
+  adapter: vercel()
 });
